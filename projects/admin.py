d6a05b2f7ace96a845556304c8efc400fca6fffe
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-08-30 19:29:22 +0300

    Add user login/out interfaces

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from .models import Task, Department,TeamMember
+from .models import Task, Department, TeamMember
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "starting_point", "goal", "satisfaction_rate")
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "head", "stuff")
 
-admin.site.register(Task)
-admin.site.register(Department)
-admin.site.register(TeamMember)
+class TeamAdmin(admin.ModelAdmin):
+    filter_horizontal = ("task",)
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(TeamMember, TeamAdmin)
