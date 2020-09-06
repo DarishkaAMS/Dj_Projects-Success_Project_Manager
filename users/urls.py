ed1b23d490262718d0e58e2f50b6f92423de465b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-09-06 06:35:15 +0300

    Add CSS through static

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 urlpatterns = [
     path("", views.index, name="indeX"),
     path("login", views.login_view, name="login"),
