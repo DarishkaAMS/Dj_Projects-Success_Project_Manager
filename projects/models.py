898bd80c7915f66b105996a3b5e821af0e6095a9
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-09-12 08:01:40 +0300

    Add name line in models

--- projects/models.py
+++ projects/models.py
@@ -12,6 +12,7 @@ class Task(models.Model):
     starting_point = models.ForeignKey(Department, on_delete = models.CASCADE, related_name="task_at_hand")
     goal = models.ForeignKey(Department, on_delete=models.CASCADE, related_name="requested_task")
     days_to_complete = models.IntegerField()
+    # name_task = models.CharField(max_length=24)
 
     def __str__(self):
         return f"{self.starting_point} request for {self.goal}" #{self.id}: 
